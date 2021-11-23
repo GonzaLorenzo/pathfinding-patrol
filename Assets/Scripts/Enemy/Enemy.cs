049554ad55f68231b9eed995d72d51b72c2aa187
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2021-11-23 04:03:17 -0300

    Field of View funcionando

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -15,8 +15,6 @@ public class Enemy : MonoBehaviour
     public LayerMask targetMask;
     public LayerMask obstacleMask;
 
-    public Color targetDefaultColor;
-    private List<GameObject> _targetsFound = new List<GameObject>();
     void Start()
     {
         _fsm = new StateMachine();
@@ -27,7 +25,7 @@ public class Enemy : MonoBehaviour
 
     void Update()
     {
-        
+        FieldOfView();
     }
 
     /// <summary>
@@ -35,7 +33,7 @@ public class Enemy : MonoBehaviour
     /// </summary>
     public void Patrol()
     {
-        Vector3 dir = waypoints[_currentWaypoint].position - transform.position;
+        Vector2 dir = waypoints[_currentWaypoint].position - transform.position;
         transform.forward = dir;
         transform.position += transform.forward * speed * Time.deltaTime;
 
@@ -46,37 +44,32 @@ public class Enemy : MonoBehaviour
                 _currentWaypoint = 0;
         }
     }
-
-    private void ClearFoundTargetList()
-    {
-        foreach (var item in _targetsFound)
-        {
-            item.GetComponent<Renderer>().material.color = targetDefaultColor;
-        }
-        _targetsFound.Clear();
-    }
     void FieldOfView()
     {
-        ClearFoundTargetList();
-
-        Collider[] allTargets = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+        Collider2D[] allTargets = Physics2D.OverlapCircleAll(transform.position, viewRadius, targetMask);
 
         foreach (var item in allTargets)
         {
-            Vector3 dir = item.transform.position - transform.position;
-
-            if (Vector3.Angle(transform.forward, dir.normalized) < viewAngle / 2)
+            Debug.Log("Tengo target");
+            Vector2 dir = item.transform.position - transform.position;
+            
+            if (Vector2.Angle(transform.forward, dir.normalized) < viewAngle / 2)
             {
-                if(Physics.Raycast(transform.position, dir, out RaycastHit hit, dir.magnitude, obstacleMask) == false)
+                //if(Physics2D.Raycast(transform.position, dir, dir.magnitude, obstacleMask) == false)
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, dir.magnitude, obstacleMask);
+                if(hit == false)
                 {
                     item.GetComponent<Renderer>().material.color = Color.red;
-                    _targetsFound.Add(item.gameObject);
+                    //_targetsFound.Add(item.gameObject);
 
-                    Debug.DrawLine(transform.position, item.transform.position, Color.red);
+                    Debug.DrawLine(transform.position, item.transform.position, Color.green);
+
+                    Debug.Log("Te encontré");
                 }
                 else
                 {
                     Debug.DrawLine(transform.position, hit.point, Color.red);
+                    Debug.Log("Donde tas?");
                 }
             }
         }
